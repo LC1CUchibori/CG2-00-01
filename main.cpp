83f1bc41e35cbded49e71f31caf01bc290f4bd49
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-08-02 16:27:39 +0900

    評価課題3

--- main.cpp
+++ main.cpp
@@ -920,7 +920,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #pragma region ModelDataを使う
 	// モデルデータ読み込み
-	ModelData modelData = LoadObjFile("resources", "axis.obj");
+	ModelData modelData = LoadObjFile("resources", "plane.obj");
 	// 頂点リソースを作成
 	Microsoft::WRL::ComPtr<ID3D12Resource> vertexModelResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
 	// 頂点バッファビューを作成する
@@ -1238,6 +1238,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				ImGui::DragFloat2("UVScale", &uvTransformSprite.scale.x, 0.01f, -10.0f, 10.0f);
 				ImGui::SliderAngle("UVRotate", &uvTransformSprite.rotate.z);
 			}
+			if (ImGui::CollapsingHeader("SpriteTransform")) {
+				ImGui::DragFloat2("UVTranslate", &transformSprite.translata.x, -5.0f,5.0f);
+				ImGui::DragFloat2("UVScale", &transformSprite.scale.x, 0.01f, -10.0f, 10.0f);
+				ImGui::SliderAngle("UVRotate", &transformSprite.rotate.z);
+			}
 			// 切り替え
 			ImGui::Checkbox("useMonsterBall", &useMonsterBall);
 			// リセット
@@ -1322,7 +1327,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
 			commandList->SetGraphicsRootConstantBufferView(0, materialResourceSprite->GetGPUVirtualAddress());
 			//描画！
-			//commandList->DrawInstanced(6, 1, 0, 0);
+			commandList->DrawInstanced(6, 1, 0, 0);
 #pragma endregion
 
 
