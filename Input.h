ec115cc617b45f0597d0ecb4d2355b8d51197647
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-10-28 14:05:05 +0900

    GE3_02-03完了

--- Input.h
+++ Input.h
@@ -9,7 +9,6 @@ class Input
 {
 public: // メンバ関数
 	template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
-	ComPtr<IDirectInput8> directInput;
 public:
     // 初期化
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
@@ -22,6 +21,7 @@ public:
 private: // メンバ変数
 // キーボードのデバイス
 	ComPtr<IDirectInputDevice8> keyboard;
+	ComPtr<IDirectInput8> directInput;
 
 	BYTE key[256] = {};
 	BYTE keyPre[256] = {};
