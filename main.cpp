d5204096e2783d7982071e51f38b9dc48827c078
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-12-06 10:26:47 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1318,6 +1318,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				Matrix4x4 worldMatrix = MakeAffineMatrix(particles[index].transform.scale, particles[index].transform.rotate, particles[index].transform.translata);
 				Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 				Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix,viewProjectionMatrix );
+				Matrix4x4 backToFromMatrix = MakeRatateYMatrix(std::numbers::pi_v<float>);
+				Matrix4x4 billboardMatrix = Multiply(backToFromMatrix, cameraMatrix);
+				billboardMatrix.m[3][0] = 0.0f;
+				billboardMatrix.m[3][1] = 0.0f;
+				billboardMatrix.m[3][1] = 0.0f;
+				Matrix4x4 worldMatrix
 				particles[index].transform.translata += particles[index].velocity * kDeltaTime;
 				particles[index].currentTime += kDeltaTime;
 				instancingData[numInstance].WVP = worldViewProjectionMatrix;
